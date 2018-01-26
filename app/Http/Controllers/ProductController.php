655c2405a2e0f4baa57b97eb207226e29355350d
ghkomitas <ghkomgh@gmail.com>
2018-01-26 15:00:37 +0400

    Connect Auth user with his product and give exception

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ProductNotBelongsToUser;
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
@@ -94,6 +96,7 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
+        $this->productUserCheck($product);
         $request['detail'] = $request->description;
         unset($request['description']);
         $product->update($request->all());
@@ -110,6 +113,15 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
+        $this->productUserCheck($product);
         $product->delete();
-        return response(null,Response::HTTP_NO_CONTENT);    }
+        return response(null,Response::HTTP_NO_CONTENT);
+    }
+
+    public function productUserCheck($product){
+
+        if (Auth::id() !== $product->user_id){
+            throw new ProductNotBelongsToUser;
+        }
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        factory(\App\User::class,5)->create();
         factory(\App\Models\Product::class,50)->create();
         factory(\App\Models\Review::class,300)->create();
         // $this->call(UsersTableSeeder::class);
